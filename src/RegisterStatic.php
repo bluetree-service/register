1dff9fa7e879d0293f715d5ca01d66c8d7b10281
chajr <chajr@bluetree.pl>
2016-10-27 21:13:21 +0200

    Added some basic files

---
+++ src/RegisterStatic.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BlueTree;
+
+class RegisterStatic
+{
+    /**
+     * @var null|Register
+     */
+    protected static $_instance = null;
+
+    /**
+     * 
+     */
+    public static function factory()
+    {
+        
+    }
+}
